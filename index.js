edd7c1ec58032aea7237675002cb6a4ad91589d2
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-05 19:51:26 +0530

    Create Player Object with index.js ...

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ const  c= canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+class Player{
+    constructor() {
+        //player Location
+        this.position={
+            x: 200,
+            y:200
+        }
+        //player speed
+        this.velocity ={
+
+        }
+
+        // for player image
+        this.width =100
+        this.height = 100
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x,this.position.y,this.width,this.height);
+    }
+}
+
+const player = new Player();
+player.draw();
