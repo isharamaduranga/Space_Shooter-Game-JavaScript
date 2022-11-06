46bcd5914947dfbd4bea889854352664d343b014
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-06 16:40:51 +0530

    Added new effect of player rotation with canvas..

--- index.js
+++ index.js
@@ -7,25 +7,28 @@ canvas.height = innerHeight;
 class Player {
     constructor() {
 
-/** player speed */
+        /** player speed */
         this.velocity = {
             x: 0,
             y: 0
         }
 
-/** Customized for player varieties */
+        this.rotation = 0;
+
+
+        /** Customized for player varieties */
         const image = new Image();
-        image.src = './img/Space_Jet.png'
+        image.src = './img/jet.png'
         image.onload = () => {
-            const scale = 0.25
+            const scale = 0.10
             this.image = image
             this.width = image.width * scale;
             this.height = image.height * scale;
 
-/** player Location and Adjust center of canvas */
+            /** player Location and Adjust center of canvas */
             this.position = {
                 x: canvas.width / 2 - this.width / 2,
-                y: canvas.height - this.height - 20
+                y: canvas.height - this.height - 10
             }
         }
     }
@@ -34,6 +37,18 @@ class Player {
         /*  c.fillStyle = 'red'
           c.fillRect(this.position.x,this.position.y,this.width,this.height);*/
 
+        c.save()
+
+        c.translate(
+            player.position.x + player.width / 2,
+            player.position.y + player.height / 2)
+
+            c.rotate(this.rotation)
+
+        c.translate(
+            -player.position.x - player.width / 2,
+            -player.position.y - player.height / 2)
+
         c.drawImage(
             this.image,
             this.position.x,
@@ -41,9 +56,12 @@ class Player {
             this.width,
             this.height
         )
+
+        c.restore();
+
     }
 
-/** Press (Arrow keys & Shift update player position ..) */
+    /** Press (Arrow keys & Shift update player position ..) */
     update() {
         if (this.image) {
             this.draw()
@@ -54,14 +72,14 @@ class Player {
 
 const player = new Player();
 const keys = {
-    ArrowLeft:{
-        pressed:false
+    ArrowLeft: {
+        pressed: false
     },
-    ArrowRight:{
-        pressed:false
+    ArrowRight: {
+        pressed: false
     },
-    Shift:{
-        pressed:false
+    Shift: {
+        pressed: false
     }
 
 }
@@ -73,15 +91,15 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
 
-    if(keys.ArrowLeft.pressed && player.position.x >=0) {
-        player.velocity.x = -5
-    }
-    else if(keys.ArrowRight.pressed && player.position.x+player.width <= canvas.width ) {
-        player.velocity.x = 5
-    }
-
-    else{
+    if (keys.ArrowLeft.pressed && player.position.x >= 0) {
+        player.velocity.x = -7
+        player.rotation =  -0.20
+    } else if (keys.ArrowRight.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 7
+        player.rotation =  0.20
+    } else {
         player.velocity.x = 0
+        player.rotation = 0
     }
 
 }
