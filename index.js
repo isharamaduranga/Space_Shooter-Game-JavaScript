a4c2c6145b2aff65246cdcf43ba62802e0731b42
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-07 16:36:08 +0530

    Implemnet space invedors and create a invedors class and content updated....

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+// ================================================================================================== //
+/** PLAYER CLASS */
+// ================================================================================================== //
 class Player {
     constructor() {
 
@@ -12,9 +15,7 @@ class Player {
             x: 0,
             y: 0
         }
-
         this.rotation = 0;
-
 
         /** Customized for player varieties */
         const image = new Image();
@@ -36,14 +37,13 @@ class Player {
     draw() {
         /*  c.fillStyle = 'red'
           c.fillRect(this.position.x,this.position.y,this.width,this.height);*/
-
         c.save()
 
         c.translate(
             player.position.x + player.width / 2,
             player.position.y + player.height / 2)
 
-            c.rotate(this.rotation)
+        c.rotate(this.rotation)
 
         c.translate(
             -player.position.x - player.width / 2,
@@ -56,9 +56,7 @@ class Player {
             this.width,
             this.height
         )
-
         c.restore();
-
     }
 
     /** Press (Arrow keys & Shift update player position ..) */
@@ -70,34 +68,86 @@ class Player {
     }
 }
 
-class projectile{
-    constructor({position,velocity}) {
+class projectile {
+    constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
-
-
-        this.radius =3
+        this.radius = 3
     }
 
-    draw(){
+    draw() {
         c.beginPath()
-        c.arc(this.position.x , this.position.y ,
-            this.radius ,0, Math.PI * 2)
+        c.arc(this.position.x, this.position.y,
+            this.radius, 0, Math.PI * 2)
         c.fillStyle = 'red'
         c.fill()
         c.closePath()
     }
-    update(){
+
+    update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
     }
+}
 
+// ================================================================================================== //
+/** INVADER CLASS */
+// ================================================================================================== //
+class Invader {
+    constructor({position}) {
+
+        /** player speed */
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        /** Customized for invader varieties */
+        const image = new Image();
+        image.src = './img/invader.png'
+        image.onload = () => {
+            const scale = 0.07
+            this.image = image
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+
+            /** invader Location and Adjust center of canvas */
+            this.position = {
+                x: position.x,
+                y: position.y
+            }
+        }
+    }
+
+    draw() {
+        /*  c.fillStyle = 'red'
+          c.fillRect(this.position.x,this.position.y,this.width,this.height);*/
+
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+    }
+
+    /** Press (Arrow keys & Shift update invader position ..) */
+    update({velocity}) {
+        if (this.image) {
+            this.draw()
+            this.position.x += velocity.x
+            this.position.y += velocity.y
+        }
+    }
 }
 
 
+
 const player = new Player()
-const projectiles = [ ]
+const projectiles = []
+
+
 const keys = {
     ArrowLeft: {
         pressed: false
@@ -118,16 +168,29 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
 
-    projectiles.forEach(projectile => {
-        projectile.update()
-    })
+
+    projectiles.forEach((projectile, index) => {
+
+        /** clean the Unnecessary projectiles in projectiles Array with condition */
+        if (projectile.position.y + projectile.radius <= 0) {
+            setTimeout(function () {
+                projectiles.splice(index, 1);
+            }, 0)
+        } else {
+            projectile.update()
+        }
+
+    });
+
+
+
 
     if (keys.ArrowLeft.pressed && player.position.x >= 0) {
         player.velocity.x = -7
-        player.rotation =  -0.20
+        player.rotation = -0.20
     } else if (keys.ArrowRight.pressed && player.position.x + player.width <= canvas.width) {
         player.velocity.x = 7
-        player.rotation =  0.20
+        player.rotation = 0.20
     } else {
         player.velocity.x = 0
         player.rotation = 0
@@ -140,32 +203,32 @@ animate();
 
 /** Player control keydown event Listener */
 addEventListener("keydown", ({key}) => {
-    console.log(key)
+
     switch (key) {
 
         case 'ArrowLeft':
-            console.log('turn left');
+            // console.log('turn left');
             keys.ArrowLeft.pressed = true
             break;
 
         case 'ArrowRight':
-            console.log('turn Right')
+            // console.log('turn Right')
             keys.ArrowRight.pressed = true
             break;
 
         case ' ':
-            console.log('Shoot the target');
+            // console.log('Shoot the target');
             projectiles.push(
                 new projectile({
-                position:{
-                    x:300,
-                    y:300
-                },
-                velocity:{
-                    x:0,
-                    y:0
-                }
-            }));
+                    position: {
+                        x: player.position.x + player.width / 2,
+                        y: player.position.y + player.height / 4
+                    },
+                    velocity: {
+                        x: 0,
+                        y: -10
+                    }
+                }));
             break;
     }
 });
@@ -174,17 +237,17 @@ addEventListener("keyup", ({key}) => {
     switch (key) {
 
         case 'ArrowLeft':
-            console.log('turn left');
+            //console.log('turn left');
             keys.ArrowLeft.pressed = false
             break;
 
         case 'ArrowRight':
-            console.log('turn Right')
+            //console.log('turn Right')
             keys.ArrowRight.pressed = false
             break;
 
         case ' ':
-            console.log('Shoot the target');
+            //console.log('Shoot the target');
             break;
     }
 });
