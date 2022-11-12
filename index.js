60790fc2f54923ab281f467b09dc3e6661b677f9
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-12 08:26:30 +0530

    created draw method for build InvaderProjectiles design ...

--- index.js
+++ index.js
@@ -108,7 +108,14 @@ class InvaderProjectile {
     }
 
     draw() {
-
+        c.fillStyle = 'white'
+        c.fillRect(this.position.x,this.position.y,this.width,this.height)
+       /* c.beginPath()
+        c.arc(this.position.x, this.position.y,
+            this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()*/
     }
 
     update() {
