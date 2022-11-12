e01b8ee436062fd71d01fa545d8dc91d6a9e51e2
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-12 22:22:09 +0530

    replaced InvaderProjectile Class with js in to classes package and import js file with index.hrml..

--- index.js
+++ index.js
@@ -141,41 +141,6 @@ class Particle {
     }
 }
 
-
-// ================================================================================================== //
-/**                               INVADER PROJECTILES CLASS                                           */
-// ================================================================================================== //
-class InvaderProjectile {
-    constructor({position, velocity}) {
-        this.position = position
-        this.velocity = velocity
-        // for size of bullet
-        this.width = 4
-        this.height = 12
-    }
-
-    draw() {
-        c.fillStyle = 'yellow'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        /* c.beginPath()
-         c.arc(this.position.x, this.position.y,
-             this.radius, 0, Math.PI * 2)
-         c.fillStyle = 'red'
-         c.fill()
-         c.closePath()*/
-    }
-
-    update() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-    }
-}
-
-
-
-
-
 const player = new Player();
 const projectiles = [];
 const grids = [];
