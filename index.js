ecaf366691ab23a8cf6a01419f65d700a44f82da
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-12 21:23:54 +0530

    Change project root & introduced new package 'classes' with js ...

--- index.js
+++ index.js
@@ -237,51 +237,7 @@ class Invader {
     }
 }
 
-class Invader_Grid {
-    constructor() {
 
-        this.position = {
-            x: 0,
-            y: 0
-        }
-
-        this.velocity = {
-            x: 3,
-            y: 0
-        }
-        this.invaders = []
-
-        /** Randomly changed grid columns  & rows , count and position */
-        const column = Math.floor(Math.random() * 10 + 5);
-        const rows = Math.floor(Math.random() * 5 + 2);
-
-        this.width = column * 35
-        /** Randomly Defined grid columns  & rows in canvas */
-        for (let x = 0; x < column; x++) {
-            for (let y = 0; y < rows; y++) {
-                this.invaders.push(new Invader({
-                    position: {
-                        x: x * 35,
-                        y: y * 35
-                    }
-                }))
-            }
-        }
-        //console.log(this.invaders)
-    }
-
-    update() {
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.velocity.y = 0
-
-        if (this.position.x + this.width >= canvas.width ||
-            this.position.x <= 0) {
-            this.velocity.x = -this.velocity.x
-            this.velocity.y = 30
-        }
-    }
-}
 
 const player = new Player();
 const projectiles = [];
