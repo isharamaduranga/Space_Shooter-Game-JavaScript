4d23a150c7ecf857ec40e86877d0ecaeb88abc54
Ishara Maduranga <isharamaduranga500@gmail.com>
2022-11-22 19:45:26 +0530

    Bug fixed & content updated ..

--- index.js
+++ index.js
@@ -241,11 +241,9 @@ function animate() {
             projectiles.forEach((projectile, p) => {
                 if (
                     projectile.position.y - projectile.radius <=
-                    invader.position.y + invader.height &&
-                    projectile.position.x + projectile.radius >=
+                    invader.position.y + invader.height && projectile.position.x + projectile.radius >=
                     invader.position.x && projectile.position.x -
-                    projectile.radius <= invader.position.x + invader.width &&
-                    projectile.position.y + projectile.radius >= invader.position.y
+                    projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y
 
                 ) {
 
